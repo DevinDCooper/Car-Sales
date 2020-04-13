f93e2a7d7ecf709a272ac1c84d742c09475cdf41
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-04-13 10:12:32 -0700

    MVP

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
 import React from 'react';
-
+import { connect } from "react-redux";
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
+import { addFeature } from './actions';
+import { removeFeature } from './actions';
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
+
+
+const App = (state) => {
 
   const removeFeature = item => {
     // dispatch an action here to remove an item
+    removeFeature(item);
   };
 
   const buyItem = item => {
     // dipsatch an action here to add an item
+    addFeature(item);
   };
 
   return (
@@ -38,11 +28,21 @@ const App = () => {
         <AddedFeatures car={state.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={state.additionalFeatures} />
+        <AdditionalFeatures additionalFeatures={state.additionalFeatures} 
+           addFeature = {buyItem}
+           removeFeature = {removeFeature}  />
         <Total car={state.car} additionalPrice={state.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    additionalFeatures: state.additionalFeatures
+  };
+}
+
+export default connect(mapStateToProps, {addFeature , removeFeature })(App);
